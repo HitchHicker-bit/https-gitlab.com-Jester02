3689d1479f678f1caf54285a3b2cfdd89eccc3a5
Лим Алексей Вячеславович <lfominych@bk.ru>
2020-01-26 23:17:32 +0000

    Update Lesson_2/homeworks/1.js

--- Lesson_2/homeworks/1.js
+++ Lesson_2/homeworks/1.js
@@ -38,3 +38,44 @@
       }
 
   */
+   var buttonContainer = document.getElementById('buttonContainer');
+  var showButton = buttonContainer.querySelectorAll('.showButton');
+  var tabContainer = document.getElementById('tabContainer');
+  var tab1 = tabContainer.querySelector('.tab[data-tab="1"]');
+  var tab2 = tabContainer.querySelector('.tab[data-tab="2"]');
+  var tab3 = tabContainer.querySelector('.tab[data-tab="3"]');
+  console.log(tab1);
+  showButton.forEach(function(item){
+    console.log(item);
+    item.onclick =function(){
+      var tab = item.getAttribute('data-tab');
+    if (tab == 1){
+      tab1.classList.add('active');
+    }
+    if (tab == 2){
+      tab2.classList.add('active');
+    }
+    if (tab == 3){
+      tab3.classList.add('active');
+    }
+   }
+  });
+    var button = document.createElement('button');
+        button.type = "button";
+        button.className = "hideAllTabs";
+        button.id = "hideAll";
+        button.innerText = "ByeCats";
+        button.style.border = "none";
+        buttonContainer.appendChild(button);
+    var hideCats = document.getElementById('hideAll');
+        hideCats.onclick = function(){  
+         if (tab1.classList.contains('active') == true) {
+          tab1.classList.toggle('active');
+         } 
+         if (tab2.classList.contains('active') == true) {
+          tab2.classList.toggle('active');
+         }
+         if (tab3.classList.contains('active') == true) {
+          tab3.classList.toggle('active');
+         }
+        }
