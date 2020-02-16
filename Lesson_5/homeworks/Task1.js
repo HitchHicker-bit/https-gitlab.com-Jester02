60eb462f9fde4d955e9fab8a0313b4019e5cf905
Hecc_ <datsenko.andrew@gmail.com>
2020-02-16 17:33:57 +0200

    5 hw

--- Lesson_5/homeworks/Task1.js
+++ Lesson_5/homeworks/Task1.js
@@ -31,41 +31,77 @@
 
     <div id="CommentsFeed"></div>
 */
-  function Submits (name,text,URL){
-      this.name = name;
-      this.text = text; 
-      if (URL !== undefined){
-        this.URL = URL;
+  // function Submits (name,text,URL){
+  //     this.name = name;
+  //     this.text = text; 
+  //     if (URL !== undefined){
+  //       this.URL = URL;
+  //     }
+  //     this.likes = 0;
+  // }
+  // var Submitty = new Submits("Dima","What's up guys?");
+  // let defUrl = {
+  //   URL:"https://vk.com/id331833459?z=photo331833459_456240917%2Falbum331833459_0%2Frev",
+  //   iLike :function(like){
+  //       if (like == "like"){
+  //         this.likes += 1;
+  //       }
+  //     }
+  // }
+  // var CommentsArray = [];
+  // function Comments (comment){
+  //   this.comment = comment;
+  //   CommentsArray.push(this.comment);
+  //   Object.setPrototypeOf(Submitty , defUrl);
+  // }
+  // var myComment1 = new Comments("Hey what about ");
+  // var myComment2 = new Comments("i'll be mushroom ");
+  // var myComment3 = new Comments("which jumping on ");
+  // var myComment4 = new Comments("Super Mario?");
+  // Submitty.iLike("like");
+  // console.log(CommentsArray);
+  // function CommentFeed (object){
+  //   for (key in object){
+  //     alert(object[key]);
+  //   }
+  // }
+  // CommentFeed(CommentsArray);
+
+
+  const feed = [];
+  const proto_obj = {
+      avatar: "https://image.flaticon.com/icons/svg/1099/1099983.svg",
+      addLike: function(){
+        this.likes ++;
+        console.log( this );
       }
-      this.likes = 0;
   }
-  var Submitty = new Submits("Dima","What's up guys?");
-  let defUrl = {
-    URL:"https://vk.com/id331833459?z=photo331833459_456240917%2Falbum331833459_0%2Frev",
-    iLike :function(like){
-        if (like == "like"){
-          this.likes += 1;
-        }
-      }
-  }
-  var CommentsArray = [];
-  function Comments (comment){
-    this.comment = comment;
-    CommentsArray.push(this.comment);
-    Object.setPrototypeOf(Submitty , defUrl);
-  }
-  var myComment1 = new Comments("Hey what about ");
-  var myComment2 = new Comments("i'll be mushroom ");
-  var myComment3 = new Comments("which jumping on ");
-  var myComment4 = new Comments("Super Mario?");
-  Submitty.iLike("like");
-  console.log(CommentsArray);
-  function CommentFeed (object){
-    for (key in object){
-      alert(object[key]);
+
+  function Comment( name, text, avatar ){
+    this.name = name;
+    this.text = text;
+    if( avatar ){
+      this.avatar = avatar;
     }
+    this.likes = 0;
+    Object.setPrototypeOf( this, proto_obj );
+
+    feed.push( this );
+    console.log( this );
   }
-  CommentFeed(CommentsArray);
 
+  let myComment1 = new Comment("test",  "Hey what about");
+  let myComment2 = new Comment("test2", "i'll be mushroom ", "https://image.flaticon.com/icons/svg/1099/1099980.svg");
+  let myComment3 = new Comment("test3", "which jumping on", "https://image.flaticon.com/icons/svg/1100/1100000.svg");
+  let myComment4 = new Comment("test4", "Super Mario?");
 
- 
+  feed.map( item => {
+    let node = document.createElement('div');
+    node.innerHTML = `
+      <h1>${item.name} (${item.likes})</h1>
+      <img width="40" src="${item.avatar}"/>
+      <p>${item.text}</p>
+    `;
+    document.body.appendChild( node );
+
+  });
